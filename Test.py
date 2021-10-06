ad76157995465187d36b312aa2bdbc9b92028914
gSorax <sorarazvan98@gmail.com>
2021-10-06 11:33:50 +0300

    Putting parts of test in try catch in order to avoid time consumption if test fail

--- Test.py
+++ Test.py
@@ -1,32 +1,60 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import Select
+from selenium.common.exceptions import NoSuchElementException
+import sys
 import time
 
 driver = webdriver.Chrome()
 driver.get("http://www.bayut.com")
 
+print("Select the apartments available for buying...")
 time.sleep(1)
-driver.find_element_by_class_name("e7c6503c").click()
-driver.find_element_by_xpath('//button[text()="Buy"]').click()
+try:
+    driver.find_element_by_class_name("e7c6503c").click()
+    driver.find_element_by_xpath('//button[text()="Buy"]').click()
+except NoSuchElementException:
+    print("The Buy button could not be found")
+    driver.quit()
+    sys.exit(1)
+    
+print("Search for Dubai Marina...")
+try:
+    driver.find_element_by_class_name("a41c4dcc").send_keys("Dubai Marina")
+    time.sleep(1)
+except NoSuchElementException:
+    print("The Search field could not be found")
+    driver.quit()
+    sys.exit(1)
 
-driver.find_element_by_class_name("a41c4dcc").send_keys("Dubai Marina")
-time.sleep(1)
-driver.find_element_by_class_name("_0e756b14").click()
-driver.find_element_by_xpath('//a[text()="Find"]').click()
+print("Find apartments for specified criteria...")
+try:
+    driver.find_element_by_class_name("_0e756b14").click()
+    driver.find_element_by_xpath('//a[text()="Find"]').click()
+    time.sleep(1)
+except NoSuchElementException:
+    print("The Find button could not be found")
+    driver.quit()
+    sys.exit(1)
 
-time.sleep(2)
-main=driver.find_element_by_class_name("bbfbe3d2")
-time.sleep(1)
-elements=main.find_elements_by_tag_name("article")
-time.sleep(1)
+try:
+    main=driver.find_element_by_class_name("bbfbe3d2")
+    time.sleep(1)
+    elements=main.find_elements_by_tag_name("article")
+    time.sleep(1)
+except NoSuchElementException:
+    print("The articles could not be found")
+    driver.quit()
+    sys.exit(1)
+
 nr=0
+print("Verify if the results are correct...")
 for x in elements:
     if "Dubai Marina" in x.find_element_by_class_name("d6e81fd0").text:
         nr=nr+1
 if(nr==len(elements)):
-    print("Test 1 passed")
+    print("Test passed")
 else:
-    print("Test 1 failed")
+    print("Test failed")
 
 driver.quit()
--- Test2.py
+++ Test2.py
@@ -1,51 +1,90 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.action_chains import ActionChains
+from selenium.common.exceptions import NoSuchElementException
+import sys
 import time
 
 driver = webdriver.Chrome()
 driver.get("https://www.bayut.com")
-def to_rent_click():
+
+print("Moving to Popular Searches...")
+try:
     ActionChains(driver).move_to_element(driver.find_element_by_xpath('//div[text()="Popular searches in the UAE"]')).perform()
     driver.find_element_by_xpath('//div[text()="To Rent"]').click()
+    time.sleep(1)
+except NoSuchElementException:
+    print("The Popular Searches could not be found")
+    driver.quit()
+    sys.exit(1)
 
-time.sleep(1)
-to_rent_click()
+print("Expanding Popular Searches...")
+try:
+    main=driver.find_element_by_class_name("fc910dcd")
+    el=main.find_elements_by_class_name("_2f838ff4")
+    for x in el:
+        x.click()
+except NoSuchElementException:
+    print("The Popular Searches could not be found")
+    driver.quit()
+    sys.exit(1)
 
-time.sleep(1)
-main=driver.find_element_by_class_name("fc910dcd")
-el=main.find_elements_by_class_name("_2f838ff4")
-for x in el:
-    x.click()
+print("Creating the Dubai Apartments list from Popular Searches...")
+try:
+    list=main.find_elements_by_class_name("_5a12e6f6")
+    for x in list:
+        if x.find_element_by_class_name("_78d325fa ").text=="Dubai Apartments":
+            list_apartments=x.find_elements_by_class_name("_76ddbf32")
+except NoSuchElementException:
+    print("The Dubai Apartments field could not be found")
+    driver.quit()
+    sys.exit(1)
 
-list=main.find_elements_by_class_name("_5a12e6f6")
-for x in list:
-    if x.find_element_by_class_name("_78d325fa ").text=="Dubai Apartments":
-        list_apartments=x.find_elements_by_class_name("_76ddbf32")
+print("Creating the Dubai Apartments list from Popular Searches...")
+try:
+    list=main.find_elements_by_class_name("_5a12e6f6")
+    for x in list:
+        if x.find_element_by_class_name("_78d325fa ").text=="Dubai Apartments":
+            list_apartments=x.find_elements_by_class_name("_76ddbf32")
+except NoSuchElementException:
+    print("The Dubai Apartments field could not be found")
+    driver.quit()
+    sys.exit(1)
 
-list_verify=[]
-for x in list_apartments:
-    if "-" in x.text:
-        list_verify.append(x.text.replace("- ","("))
-    else:
-        list_verify.append(x.text)
-list_links=[]
-for x in list_apartments:
-    link=x.find_element_by_css_selector("a").get_attribute("href")
-    list_links.append(link)
+print("Preparing verify and link lists...")
+try:
+    list_verify=[]
+    for x in list_apartments:
+        if "-" in x.text:
+            list_verify.append(x.text.replace("- ","("))
+        else:
+            list_verify.append(x.text)
+    list_links=[]
+    for x in list_apartments:
+        link=x.find_element_by_css_selector("a").get_attribute("href")
+        list_links.append(link)
+except NoSuchElementException:
+    print("The links could not be found")
+    driver.quit()
+    sys.exit(1)
 
-verify=0
-i=0
-for x in list_links:
-    driver.get(x)
-    src_header=driver.find_element_by_id("search-header")
-    if("Apartments for rent in "+list_verify[i] in src_header.text):
-        verify=verify+1
-        i=i+1
-    else:
-        print(list_verify[i])
+print("Verify if the search is correct...")
+try:
+    verify=0
+    i=0
+    for x in list_links:
+        driver.get(x)
+        src_header=driver.find_element_by_id("search-header")
+        if("Apartments for rent in "+list_verify[i] in src_header.text):
+            verify=verify+1
+            i=i+1
+        else:
+            print(list_verify[i])
 
-        break
+except NoSuchElementException:
+    print("The search field could not be found")
+    driver.quit()
+    sys.exit(1)
 
 if verify==len(list_verify):
     print("Test passed")
